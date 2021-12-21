ff3c4aec25ab03f0cb1fcedc6f0d58508af1afdf
Alex Pacheco <alex.pacheco@usach.cl>
2021-12-20 22:49:31 -0300

    todas las migraciones generadas
    se generan todas las tablas, pero tan solo se agregan las columnas para "users" y "followers"

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('role',20);
+            $table->string('nickname',20);
+            $table->string('name',20);
+            $table->string('email',20);
+            $table->string('password');
+            $table->integer('wallet');
+            $table->string('ubication',20);
+            $table->date('date_of_birth');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
---
+++ database/migrations/2021_12_18_015757_create_followers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFollowersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('followers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_follower')->nullable();
+            $table->foreign('id_follower')->references('id')->on('users');
+            $table->unsignedBigInteger('id_followed')->nullable();
+            $table->foreign('id_followed')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('followers');
+    }
+}
