dbe14faf1034f58964ebde00ff97471bc6cc52f5
Nicovenegas <nicovenegas18@gmail.com>
2022-01-09 06:17:28 -0300

    PMethod controller

--- app/Http/Controllers/PaymentMethodController.php
+++ app/Http/Controllers/PaymentMethodController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\PaymentMethod;
 
 class PaymentMethodController extends Controller
 {
@@ -13,7 +14,12 @@ class PaymentMethodController extends Controller
      */
     public function index()
     {
-        //
+        $PM = new PaymentMethod();
+        $PM = $PM->all();
+        if($PM::isempty()){
+            return respose()->json(['message'=>'No data found'],404);
+        }
+        return response($PM,200);
     }
 
     /**
@@ -45,7 +51,12 @@ class PaymentMethodController extends Controller
      */
     public function show($id)
     {
-        //
+        $PM = new PaymentMethod();
+        $PM = $PM->find($id);
+        if($PM::isempty()){
+            return respose()->json(['message'=>'No data found'],404);
+        }
+        return response($PM,200);
     }
 
     /**
@@ -68,7 +79,28 @@ class PaymentMethodController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|min:2|max:100',
+            'id_user' => 'required',
+        ],
+        [
+            'name.required' => 'Name is required',
+            'name.string' => 'Name must be a string',
+            'name.min' => 'Name must be at least 2 characters',
+            'name.max' => 'Name must be at most 100 characters',
+            'id_user.required' => 'User is required',
+        ]);
+        if($validator->fails())
+            return response($validator->errors(), 400);
+        $PM = new PaymentMethod();
+        $PM = $PM->find($id);
+        if($PM::isempty()){
+            return respose()->json(['message'=>'No data found'],404);
+        }
+        $PM->name = $request->name;
+        $PM->id_user = $request->id_user;
+        $PM->save();
+        return response()->json(['message'=>'Payment Method updated successfully'],200);
     }
 
     /**
@@ -79,6 +111,21 @@ class PaymentMethodController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $validator = Validator::make(['id'=>$id], [
+            'id' => 'required|integer',
+        ],
+        [
+            'id.required' => 'Id is required',
+            'id.integer' => 'Id must be an integer',
+        ]);
+        if($validator->fails())
+            return response($validator->errors(), 400);
+        $PM = new PaymentMethod();
+        $PM = $PM->find($id);
+        if($PM::isempty()){
+            return respose()->json(['message'=>'No data found'],404);
+        }
+        $PM->delete();
+        return response()->json(['message'=>'Payment Method deleted successfully'],200);
     }
 }
