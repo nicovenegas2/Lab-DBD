416cd26b1130d9247e6816f0021d0ecabab42564
Francisco Salfate <francisco.salfate.ga@usach.cl>
2022-01-09 05:26:36 -0300

    Controlador de Message

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Message;
 
 class MessageController extends Controller
 {
@@ -13,7 +14,11 @@ class MessageController extends Controller
      */
     public function index()
     {
-        //
+        $messages = Message::all();
+        if($messages->isEmpty()) 
+        return response()->json(['response' => 'messages not founded']);
+
+        return response($messages, 200);
     }
 
     /**
@@ -34,7 +39,36 @@ class MessageController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make(
+            $request->all(),[
+                'id_sender' => 'required',
+                'id_receiver' => 'required',
+                'content' => 'required|min:2|max:5000',
+                'date' => 'required',
+            ],
+            [
+                'id_sender.required' => "Sender id is required",
+                'id_receiver.required' => 'Receiver id is required',
+                'content.required' => 'Content is required',
+                'content.min' => 'Content min chars 2',
+                'content.max' => 'Content max chars 5000',
+                'date.required' => 'Date is required',
+            ]
+            );
+            
+        if($validator->fails())
+            return response($validator->errors(), 400);
+        
+        $newmessage = new Message();
+        $newmessage->id_sender = $request->id_sender;
+        $newmessage->id_receiver = $request->id_receiver;
+        $newmessage->content = $request->content;
+        $newmessage->date = $request->date;
+        $newmessage->save();
+        return response()->json([
+            'response' => 'Message created',
+            'id' => $newmessage->id,
+        ],201);
     }
 
     /**
@@ -45,7 +79,11 @@ class MessageController extends Controller
      */
     public function show($id)
     {
-        //
+        $messages = Message::find($id);
+        if(empty($messages)) 
+        return response()->json(['response' => 'message not found']);
+
+        return response($messages,200);
     }
 
     /**
@@ -56,7 +94,39 @@ class MessageController extends Controller
      */
     public function edit($id)
     {
-        //
+        $validator = Validator::make(
+            $request->all(),[
+                'id_sender' => 'required',
+                'id_receiver' => 'required',
+                'content' => 'required|min:2|max:5000|',
+                'date' => 'required',
+            ],
+            [
+                'id_sender.required' => "Sender id is required",
+                'id_receiver.required' => 'Receiver id is required',
+                'content.required' => 'Content is required',
+                'content.min' => 'Content min chars 2',
+                'content.max' => 'Content max chars 5000',
+                'date.required' => 'Date is required',
+            ]
+            );
+            
+        if($validator->fails())
+            return response($validator->errors(), 400);
+
+        $message = Message:find($id);
+
+        if(empty($message))
+        return response()->json(['response' => 'Message not found']);
+
+        $message->id_sender = $request->id_sender;
+        $message->id_receiver = $request->id_receiver;
+        $message->content = $request->content;
+        $message->date = $request->date;
+        $message->save();
+        return response()->json([
+            'response' => 'Message modified'
+        ],200);
     }
 
     /**
@@ -79,6 +149,14 @@ class MessageController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $message = Message::find($id);
+        
+        if(empty($message))
+        return response()->json(['response' => 'Message not found']);
+
+        $message->delete();
+        return response()->json([
+            'response' => 'Message deleted'
+        ],200);
     }
 }
