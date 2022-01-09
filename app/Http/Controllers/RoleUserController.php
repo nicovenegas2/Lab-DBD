df6ac950fcaedaf29b78a5b578e344e14560ceff
Nicovenegas <nicovenegas18@gmail.com>
2022-01-09 06:43:13 -0300

    Role user Controller

--- app/Http/Controllers/RoleUserController.php
+++ app/Http/Controllers/RoleUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\RoleUser;
 
 class RoleUserController extends Controller
 {
@@ -13,7 +14,12 @@ class RoleUserController extends Controller
      */
     public function index()
     {
-        //
+        $roleUser = new RoleUser();
+        $roleUser = $roleUser->all();
+        if($roleUser::isempty()){
+            return respose()->json(['message'=>'No data found'],404);
+        }
+        return response($roleUser,200);
     }
 
     /**
@@ -34,7 +40,23 @@ class RoleUserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required',
+            'role_id' => 'required',
+        ],
+        [
+            'user_id.required' => 'User ID is required',
+            'role_id.required' => 'Role ID is required',
+        ]);
+        if($validator->fails())
+        {
+            return response($validator->errors(),400);
+        }
+        $roleUser = new RoleUser();
+        $roleUser->user_id = $request->user_id;
+        $roleUser->role_id = $request->role_id;
+        $roleUser->save();
+        return response()->json(['message'=>'User Role created successfully'],201);
     }
 
     /**
@@ -45,7 +67,12 @@ class RoleUserController extends Controller
      */
     public function show($id)
     {
-        //
+        $roleUser = new RoleUser();
+        $roleUser = $roleUser->find($id);
+        if($roleUser::isempty()){
+            return respose()->json(['message'=>'No data found'],404);
+        }
+        return response($roleUser,200);
     }
 
     /**
@@ -68,7 +95,27 @@ class RoleUserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required',
+            'role_id' => 'required',
+        ],
+        [
+            'user_id.required' => 'User ID is required',
+            'role_id.required' => 'Role ID is required',
+        ]);
+        if($validator->fails())
+        {
+            return response($validator->errors(),400);
+        }
+        $roleUser = new RoleUser();
+        $roleUser = $roleUser->find($id);
+        if($roleUser::isempty()){
+            return respose()->json(['message'=>'No data found'],404);
+        }
+        $roleUser->user_id = $request->user_id;
+        $roleUser->role_id = $request->role_id;
+        $roleUser->save();
+        return response()->json(['message'=>'User Role updated successfully'],200);
     }
 
     /**
@@ -79,6 +126,12 @@ class RoleUserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $roleUser = new RoleUser();
+        $roleUser = $roleUser->find($id);
+        if($roleUser::isempty()){
+            return respose()->json(['message'=>'No data found'],404);
+        }
+        $roleUser->delete();
+        return response()->json(['message'=>'User Role deleted successfully'],200);
     }
 }
