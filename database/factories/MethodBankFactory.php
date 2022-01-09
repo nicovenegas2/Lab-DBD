6c428cd19e0efa53ef60392b5d85cab354665cce
Nicovenegas <nicovenegas18@gmail.com>
2022-01-09 02:34:20 -0300

    factory

--- database/factories/MethodBankFactory.php
+++ database/factories/MethodBankFactory.php
@@ -3,18 +3,20 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use app\Models\MethodBank;
+use app\Models\Bank;
+use app\Models\PaymentMethod;
 
 class MethodBankFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
+    protected $model = MethodBank::class;
+
     public function definition()
     {
+        protected $model = MethodBank::class;
         return [
-            //
+            'id_bank' => Bank::all()->random()->id,
+            'id_method' => PaymentMethod::all()->random()->id
         ];
     }
 }
