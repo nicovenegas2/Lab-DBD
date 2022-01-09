2053675f2f9446f515f6adbb3d19b7354fdb77e9
Vicente <leppe.vicente@gmail.com>
2022-01-09 02:11:28 -0300

    creaciones de factorys 2.0

---
+++ database/factories/ContentVoucherFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ContentVoucher;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ContentVoucherFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = ContentVoucher::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/factories/CountryGameFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\CountryGame;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CountryGameFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = CountryGame::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
